574f5018d65775249d220bf42051688b99ce554c
flyon <flyon@umich.edu>
2023-08-14 03:34:25 -0400

    Initial commit

---
+++ window.h
@@ -0,0 +1,13 @@
+#ifndef window.h
+#define window.h
+
+// GLFW: Allows us to create a window, make a run loop, and process input
+// GLAD: Helps us retrieve the locations of function we need
+
+const int WIDTH = 800, HEIGHT = 600;
+
+void openWindow() {
+
+}
+
+#endif
